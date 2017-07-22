83afd1cd22f5ea6a4b28c1b865509e7ffc50961b
Chmen <tony.potapov@gmail.com>
2017-07-22 09:00:08 +0300

    add employee table to game table

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ActiveRecord::Base
+  def employee_name(game_id, role)
+    return nil if Assignment.find_by(role: role, game_id: game_id).nil?
+    Employee.find(Assignment.find_by(role: role, game_id: game_id).employee_id).name
+  end
 end
