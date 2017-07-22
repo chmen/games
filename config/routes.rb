5cd3f83e0ce3eaceeeecc3669b41304c3a26969e
Chmen <tony.potapov@gmail.com>
2017-07-22 09:23:38 +0300

    add associate assignments to game

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :assignments
+  
   root 'main#inbdex'
 
   #post 'employye' => 'emploeys@create'
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   #post 'game/:id' => 'game@update'
   resources :employees
-  resources :games
+  resources :games do
+    resources :assignments 
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
